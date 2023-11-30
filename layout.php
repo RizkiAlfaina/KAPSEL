a9a2b228a8477a634143c7483171623b7462f1b8
alvinyogakesuma <alvinyoga33@gmail.com>
2023-11-30 11:30:23 +0700

    layout.php

---
+++ layout.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>SMKN1 PEKALONGAN</title>
+
+    <!-- Tailwind CSS -->
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <!-- Google Fonts (Montaga, Montserrat, Source Sans 3) -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Montaga&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Source+Sans+3:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- Swipe JS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+    <!-- Font Awesome -->
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+      integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+
+    <!-- Tailwind Config -->
+    <script>
+      tailwind.config = {
+        theme: {
+          extend: {
+            colors: {},
+            fontFamily: {
+              Montserrat: ["Montserrat", "sans-serif"],
+              SourceSans: ["Source+Sans+3", "sans-serif"],
+              Montaga: ["Montaga", "sans-serif"],
+            },
+          },
+        },
+      };
+    </script>
+
+    <style type="text/tailwindcss">
+      @layer utilities {
+        .hamburger-line {
+          @apply w-[30px] h-[2px] my-2 block bg-white;
+        }
+
+        .hamburger-active > span:nth-child(1) {
+          @apply rotate-45;
+        }
+
+        .hamburger-active > span:nth-child(2) {
+          @apply scale-0;
+        }
+
+        .hamburger-active > span:nth-child(3) {
+          @apply -rotate-45;
+        }
+
+        .swiper-slide-active {
+          @apply scale-125;
+        }
+      }
+    </style>
+  </head>
+  <body class="overflow-x-hidden">
+    <header class="xl:absolute xl:z-50 w-full bg-[#655740] xl:bg-transparent flex flex-col box-border xl:flex-row xl:justify-between xl:items-center xl:px-[69px]">
+      <div class="flex justify-between px-3 py-6 items-center" id="top-bar">
+        <a href="" class="inline-block flex items-center gap-3">
+          <img class="w-[75px] xl:w-[100px] inline-block" src="assets/images/logo_sekolah.png" alt="" />
+          <div class="inline-block">
+            <h1 class="w-56 text-white text-lg font-extrabold font-Montserrat w-fit tracking-widest">SMKN 1</h1>
+            <h1 class="w-56 text-white text-lg font-extrabold font-Montserrat w-fit tracking-widest">PEKALONGAN</h1>
+            <p class="opacity-50 text-white text-xs font-semibold font-SourceSans w-fit tracking-[5px]">SMK Bisa - Hebat</p>
+          </div>
+        </a>
+        <button id="hamburger" name="hamburger" type="button" class="inline-block p-2 xl:hidden">
+          <span class="hamburger-line origin-top-left transition duration-300 ease-in-out"></span>
+          <span class="hamburger-line transition duration-300 ease-in-out"></span>
+          <span class="hamburger-line origin-bottom-left transition duration-300 ease-in-out"></span>
+        </button>
+      </div>
+      <nav class="flex flex-col px-4 justify-center gap-5 my-5 hidden xl:flex xl:flex-row xl:gap-11 xl:justify-normal xl:px-0" id="nav-menu">
+        <a href="" class="text-white font-Montserrat text-xl">PROFIL</a>
+        <a href="" class="text-white font-Montserrat text-xl">INFORMASI</a>
+        <a href="" class="text-white font-Montserrat text-xl">KEJURUAN</a>
+        <a href="" class="text-white font-Montserrat text-xl">EKSTRAKURIKULER</a>
+        <a href="" class="text-white font-Montserrat text-xl">KONTAK</a>
+      </nav>
+      <a href="" class="text-white font-Montserrat text-xl px-4 font-bold mb-5 hidden xl:block xl:mb-0" id="login">LOG IN</a>
+    </header>
+    
+    <!-- Tambahkan Carousel dan Main content pada bagian ini -->
+
+    <footer class="bg-[#655740] flex items-center flex-col gap-10 md:gap-0 md:flex-row py-10 justify-between">
+      <div class="flex items-center gap-5 md:gap-14 flex-col md:flex-row">
+        <div class="flex gap-7 border-0 md:border md:border-r-2 md:border-y-0 md:border-l-0 md:border-r-white">
+          <img class="w-[100px]" src="assets/images/logo_sekolah.png" alt="Logo_SMKN1_Pekalongan" />
+          <div class="inline-block flex flex-col justify-end">
+            <h1 class="w-56 text-white text-lg font-extrabold font-['Montserrat'] uppercase leading-[23px] tracking-widest">SMKN 1 PEKALONGAN</h1>
+            <p class="w-[185px] h-[38px] opacity-50 text-white text-xs font-semibold font-['Source Sans 3'] capitalize tracking-[5px]">SMK Bisa - Hebat</p>
+          </div>
+        </div>
+        <div class="text-white font-Montaga flex flex-col gap-2">
+          <div class="flex items-center gap-3">
+            <i class="fa-solid fa-map inline-block align-middle"></i>
+            <p class="inline-block w-72 leading-4 tracking-wide">Jl. Bengkok No.29, Sidodadi, Kec. Pekalongan, Kabupaten Lampung Timur, Lampung 34381</p>
+          </div>
+          <div class="flex items-center gap-3">
+            <i class="fa-solid fa-phone"></i>
+            <p class="inline-block">0721-8888-9999</p>
+          </div>
+        </div>
+      </div>
+      <div class="text-white flex flex-row gap-7">
+        <a href="">
+          <i class="fa-brands fa-square-facebook fa-2xl"></i>
+        </a>
+        <a href="">
+          <i class="fa-brands fa-twitter fa-2xl"></i>
+        </a>
+        <a href="">
+          <i class="fa-brands fa-instagram fa-2xl"></i>
+        </a>
+      </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <script src="assets/js/hamburger.js"></script>
+    <script src="assets/js/swiper.js"></script>
+  </body>
+</html>
